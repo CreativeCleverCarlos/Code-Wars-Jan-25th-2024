abda06e559ee9f6de264b880f4fd5144f5fdf178
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2024-01-25 15:21:11 -0500

    full kata

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+**/
+
+function litres(time) {
+    return Math.floor(time * 0.5);
+  }
